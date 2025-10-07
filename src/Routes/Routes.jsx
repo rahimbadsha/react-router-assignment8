55c3f4afea4817613dff840c7bfd5021c56346e3
Rahim <rahimbadsha.cse@gmail.com>
2025-10-07 20:14:44 +0600

    Setup the project files

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
